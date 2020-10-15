984996f1e3cbf8434f3f6df02a8998e6ebb4a2d0
TomMarazzo <camenm@hotmail.com>
2020-10-15 17:05:13 -0400

    Connect to Mongoose and some other stuff!

--- app.js
+++ app.js
@@ -9,6 +9,23 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Database Connection - try to connect an log a pass/fail result!
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://TomMarazzo:Camendog5!@clustertaskmanager.hpr9h.mongodb.net/tasks',
+   {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+   }).then(
+      (res) =>
+      {
+         console.log('Connection to MongoDB')
+      }).catch (() =>
+{
+   console.log('Connection Error')
+})
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -23,19 +40,19 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+   // set locals, only providing error in development
+   res.locals.message = err.message;
+   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+   // render the error page
+   res.status(err.status || 500);
+   res.render('error');
 });
 
 module.exports = app;
